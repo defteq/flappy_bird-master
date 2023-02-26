18a52c3d1d6b840a6660b33bfd4d8ac618375984
defteq <cickanandrej0@gmail.com>
2023-02-26 17:56:15 +0800

    !

--- flappy_bird/src/flappybird/Bird.java
+++ flappy_bird/src/flappybird/Bird.java
@@ -11,6 +11,8 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+import static flappybird.FlappyBird.HEIGHT;
+
 /**
  *
  * @author User
@@ -19,14 +21,17 @@ public class Bird {
     public float x, y, vx, vy;
     public static final int RAD = 25;
     private Image img;
-    public Bird() {
+    public void Fly() throws InterruptedException, IOException {
         x = FlappyBird.WIDTH/2;
-        y = FlappyBird.HEIGHT/2;
+        y = HEIGHT/2;
         try {
             img = ImageIO.read(new File("sticker,375x360.u2.png"));
         }
         catch(IOException e) {
             e.printStackTrace();
+        }
+        if (RAD >= HEIGHT/2){
+            img = ImageIO.read(new File("sticker,375x360.u3.png"));
         }
     }
     public void physics() {
--- flappy_bird/src/flappybird/FlappyBird.java
+++ flappy_bird/src/flappybird/FlappyBird.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
 import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -104,7 +105,11 @@ public class FlappyBird implements ActionListener, KeyListener {
 
     public void keyPressed(KeyEvent e) {
         if(e.getKeyCode()==KeyEvent.VK_Q) {
-            bird.jump();
+            try {
+                bird.Fly();
+            } catch (InterruptedException | IOException ex) {
+                throw new RuntimeException(ex);
+            }
         }
         else if(e.getKeyCode()==KeyEvent.VK_ALT) {
             paused = false;
